6a570c9728ece1f161f6c3a073c4493cdab7ac5c
pavel_shymanski <pavl1n>
2021-03-22 11:14:43 +0300

    asdq

--- spec/constants.rb
+++ spec/constants.rb
@@ -1,9 +1,9 @@
-IMG = '//div[@class="news-header__preview"]/div[@class="news-header__image"]/@style'
-ONLINER = 'https://www.onliner.by/'
-XPATH_LINK_ONE = '//div[@class="g-middle-i"]/div/div/div/div/a[@class="b-tile-main"]/@href'
-XPATH_LINK_TWO = '//div[@class="b-tiles cfix "]/div/div/div/a[@class="b-tile-main"]/@href'
-XPATH_LINK_THREE = '//div[@class="b-main-page-grid-4 b-main-page-news-2"]/ul/li/a[@class="b-teasers-2__teaser-i"]/@href'
-TITLE = '//div[@class="news-header__title"]/h1'
-TEXT = '//div[@class="news-text"]/p'
+IMG = '//div[@class="news-header__preview"]/div[@class="news-header__image"]/@style'.freeze
+ONLINER = 'https://www.onliner.by/'.freeze
+XPATH_LINK_ONE = '//div[@class="g-middle-i"]/div/div/div/div/a[@class="b-tile-main"]/@href'.freeze
+XPATH_LINK_TWO = '//div[@class="b-tiles cfix "]/div/div/div/a[@class="b-tile-main"]/@href'.freeze
+XPATH_LINK_THREE = '//div[@class="b-main-page-grid-4 b-main-page-news-2"]/ul/li/a[@class="b-teasers-2__teaser-i"]/@href'.freeze
+TITLE = '//div[@class="news-header__title"]/h1'.freeze
+TEXT = '//div[@class="news-text"]/p'.freeze
 TEXT_REGULAR = /[^\s.,!?0-9А-Яа-я]/
 IMG_REGULAR = /\w+-\w+:\s\w+/
